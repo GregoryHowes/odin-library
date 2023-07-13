56ca7ee6dc012a63034087a94b6f1df010de5f5d
Greg Howes <gihowes@gmail.com>
2023-07-13 11:36:59 +0900

    Bodged a way to get modal to appear centrally

--- script.js
+++ script.js
@@ -8,5 +8,24 @@ function Book (title, author, pages, read) {
     }
 }
 
-const theHobbit = new Book("The Hobbit", "Tolkien", 295, false);
-console.log(theHobbit.info());
+//const theHobbit = new Book("The Hobbit", "Tolkien", 295, false);
+//console.log(theHobbit.info());
+
+
+//Modal stuff for adding new book
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector("#add-book");
+const closeModalBtn = document.querySelector(".btn-close");
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+closeModalBtn.addEventListener("click", closeModal);
